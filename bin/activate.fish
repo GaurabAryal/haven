35405a68f5f872c577d19a1e6bf61c400753e450
Gaurab Aryal <gaurab.aryal7@gmail.com>
2020-01-16 11:22:19 -0500

    Update the repo

--- bin/activate.fish
+++ bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/gaurabaryal/Documents/Programming/haven"
+set -gx VIRTUAL_ENV "/Users/gaurabaryal/Documents/Programming/haven/haven"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
