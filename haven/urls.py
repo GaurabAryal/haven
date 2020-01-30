69c04fcf174a093a9704f330391df62a67bf5daa
Brian Chen <brianchen1997@gmail.com>
2020-01-30 00:23:04 -0500

    Initial Deployment to Digital Ocean (#8)

--- haven/urls.py
+++ haven/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 from havenapp import schema
 from graphene_django.views import GraphQLView
 
 
 urlpatterns = [
-    path('graphql/', GraphQLView.as_view(graphiql=True)),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
