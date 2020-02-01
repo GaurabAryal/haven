3ecafcf1e29138cbc55b9001c82e808ab21d9a98
jeremyukim <jeremyukim@gmail.com>
2020-02-01 18:29:49 -0500

    Add login and util styles

---
+++ client/src/components/TextInput/TextInput.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import 'src/utils.css'
+import './TextInput.css';
+
+export default function TextInput(props) {
+  return (
+    <div className={props.isHalfWidth ? 'text-input-container flex-1' : 'text-input-container'}>
+      <label for={props.label} class="text-input__label text--sm font-weight--semi-bold">{props.label}</label>
+      <input
+        type={props.type || 'text'}
+        value={props.value || ''}
+        isHalfWidth={props.isHalfWidth || false}
+        placeholder={props.placeholder || ''}
+        onChange={props.onChange}
+      />
+    </div>
+  );
+}
+
+TextInput.propTypes = {
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  isHalfWidth: PropTypes.bool,
+  placeholder: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+};
