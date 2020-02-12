b519ef3b92a7e766c9717f7f8fac8312158efb51
James Kim <jamesyk1997@gmail.com>
2020-02-12 14:58:21 -0500

    users who created profile get sent to waiting screen

--- client/src/Views/Onboarding/OnboardingContainer.js
+++ client/src/Views/Onboarding/OnboardingContainer.js
@@ -1,8 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import { graphql } from '@apollo/react-hoc';
 import { compose } from 'recompose';
 import gql from 'graphql-tag';
+import { Query } from '@apollo/react-components';
 
 import OnboardingPage from './OnboardingPage';
+
+const GET_USER_QUERY = gql`
+  {
+    me {
+      profile {
+        onboardingDone
+      }
+    }
+  }
+`;
 
 const CREATE_PROFILE_MUTATION = gql`
   mutation CreateProfileMutation(
@@ -28,6 +41,30 @@ const CREATE_PROFILE_MUTATION = gql`
   }
 `;
 
+const OnboardingPageContainer = props => (
+  <Query query={GET_USER_QUERY}>
+    {({ loading, error, data }) => {
+      if (loading) return 'Loading...';
+      if (error) return `Error! ${error.message}`;
+
+      const isOnboardingDone =
+        data.me?.profile?.onboardingDone || false;
+
+      return (
+        <OnboardingPage
+          firstName="James"
+          initialStep={isOnboardingDone ? 3 : null}
+          createProfileMutation={props.createProfileMutation}
+        />
+      );
+    }}
+  </Query>
+);
+
+OnboardingPageContainer.propTypes = {
+  createProfileMutation: PropTypes.func,
+};
+
 export default compose(
   graphql(CREATE_PROFILE_MUTATION, { name: 'createProfileMutation' }),
-)(OnboardingPage);
+)(OnboardingPageContainer);
