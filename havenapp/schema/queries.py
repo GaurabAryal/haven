c6a73cecf243415b69816db07676ac91dabb6e95
Brian Chen <brianchen1997@gmail.com>
2020-02-26 17:36:49 -0500

    Added group query

--- havenapp/schema/queries.py
+++ havenapp/schema/queries.py
@@ -11,9 +11,13 @@ class GroupQuery(graphene.AbstractType):
     groups = graphene.List(GroupNode)
     membership = graphene.List(GroupNode)
     group_users = graphene.List(UserNode)
+    group = graphene.Field(GroupNode, group_id=graphene.String())
 
     def resolve_groups(self, info, **kwargs):
         return Group.objects.all()
+
+    def resolve_group(self, info, group_id):
+        return Group.objects.get(id=group_id)
 
     def resolve_membership(self, info, **kwargs):
         user = info.context.user
@@ -28,7 +32,6 @@ class GroupQuery(graphene.AbstractType):
             raise GraphQLError('User is not logged in!')
 
         members = user.group_set.first().members.all()
-        print(members)
         return members
 
 # Queries related to Users
