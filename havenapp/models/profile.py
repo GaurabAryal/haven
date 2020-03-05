15e7137474f509f53ed8b13f1279abe22a096b2c
Brian Chen <brianchen1997@gmail.com>
2020-03-04 19:59:18 -0500

    Initial work for profile pic and other profile stuff

--- havenapp/models/profile.py
+++ havenapp/models/profile.py
@@ -1,16 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
+from haven.storage_backends import PublicMediaStorage
 
 # Profile Model
+
+def get_upload_to(instance, filename):
+    return f'profile/{instance.user.id}/{filename}'
+
 class Profile(models.Model):
     # Declare types of user status
     NORMAL_USER = 1
     SEARCHING = 2
     NEWLY_MATCHED = 3
+    BANNED = 4
     STATUS = (
         (NORMAL_USER, 'Normal user'),
         (SEARCHING, 'Searching for group'),
         (NEWLY_MATCHED, 'Matched with new group'),
+        (BANNED, 'User is banned'),   
     )
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -21,6 +28,6 @@ class Profile(models.Model):
         choices=STATUS,
         default=SEARCHING, 
     )
-
-    # country = models.CharField(max_length=50, null=True)
-    # city = models.CharField(max_length=50, null=True)
+    is_verified = models.BooleanField(default=False)
+    profile_picture = models.ImageField(upload_to=get_upload_to, null=True)
+    
